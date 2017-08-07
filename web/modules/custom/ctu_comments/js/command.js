a29c5fbea1868e0ab08e67ff5b06df4f01789437
Jesmaster <jesmasterha@gmail.com>
2017-08-06 20:17:29 -0400

    Updates, cleanup, footnotes module

--- web/modules/custom/ctu_comments/js/command.js
+++
@@ -1,6 +0,0 @@
-(function($, Drupal) {
-    Drupal.AjaxCommands.prototype.ctuComments = function(ajax, response, status){
-        $('#current-msg h2').html(response.subject);
-        $('#current-msg p').html(response.content);
-    }
-})(jQuery, Drupal);
